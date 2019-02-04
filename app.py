0d68358ccd3405fb29a4d8988be1f667f3da510c
medvecky <alexey.medvecky@gmail.com>
2019-02-04 23:26:31 +0200

    templates for login and signup

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == '__main__':
