ae5d1d02c6c76f2b87f5dac791ec5a4ee1be3650
Alexey Medvecky <alexey.medvecky@gmail.com>
2019-02-05 07:31:38 +0000

    basic error handling

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from helpers import apology
+from werkzeug.exceptions import default_exceptions
 
 app = Flask(__name__)
 
@@ -14,5 +16,14 @@ def signup():
     return render_template("signup.html")
 
 
+def errorhandler(e):
+    """Handle error"""
+    return apology(e.name, e.code)
+
+
+
+
 if __name__ == '__main__':
     app.run()
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
